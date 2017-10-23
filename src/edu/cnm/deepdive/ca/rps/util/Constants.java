72b5265b0085d2e023f50aa7ea09f0ba8bde878b
cyeef <cyeef@icloud.com>
2017-10-23 16:04:50 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/ca/rps/util/Constants.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.ca.rps.util;
+
+public class Constants {
+
+  public static final String RESOURCES_UI_STRINGS = "resources/ui-strings";
+  public static final String RESOURCES_MAIN_FXML = "resources/main.fxml";
+  public static final String WINDOW_TITLE = "windowTitle";
+
+  public static final String WINDOW_TITLE_KEY = "windowTitle";
+  public static final int WINDOW_WINDOW = 640;
+  public static final int WINDOW_HEIGHT = 750;
+
+}
---
+++ src/edu/cnm/deepdive/ca/rps/views/Timer.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.ca.rps.views;
+
+import edu.cnm.deepdive.ca.rps.models.Terrain;
+import javafx.animation.AnimationTimer;
+
+public class Timer extends AnimationTimer {
+
+  private TerrainView view;
+
+  public Timer(TerrainView view) {
+    this.view = view;
+  }
+  @Override
+  public void handle(long now) {
+    view.draw();
+  }
+
+}
+
